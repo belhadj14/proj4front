c22aa92a1d53b4c41430712c84fe58188ac73dff
belhadj14 <belhadjt@gmail.com>
2025-09-18 17:10:33 +0200

    cc

--- data.js
+++ data.js
@@ -4,21 +4,24 @@ const products = [
     name: "Nespresso Essenza Mini",
     category: "Kitchen Appliances",
     description: "Compact coffee maker with fast heat-up and easy capsule system.",
-    image: "images2/nespresso.jpg"
+    image: "images2/nespresso.jpg",
+    price:"170"
   },
   {
     id: 2,
     name: "Philips ProBlend 750W",
     category: "Kitchen Appliances",
     description: "Powerful blender with multiple speed settings and durable blades.",
-    image: "images2/blender.jpg"
+    image: "images2/blender.jpg",
+    price:"120"
   },
   {
     id: 3,
     name: "Tefal Non-Stick Frying Pan 28cm",
     category: "Cookware",
     description: "Durable non-stick frying pan with Thermo-Spot heat indicator.",
-    image: "images2/tefal.jpg"
+    image: "images2/tefal.jpg",
+    price:"100"
   },
   {
     id: 4,
@@ -32,34 +35,39 @@ const products = [
     name: "Porcelain Dinner Plate Set (6pcs)",
     category: "Tableware & Dining",
     description: "Elegant white porcelain dinner plates, microwave and dishwasher safe.",
-    image: "images2/porcelaine.webp"
+    image: "images2/porcelaine.webp",
+    price:"50"
   },
   {
     id: 6,
     name: "Crystal Glasses (4pcs)",
     category: "Tableware & Dining",
     description: "Premium crystal water glasses with a sleek design for fine dining.",
-    image: "images2/crystal.jpg"
+    image: "images2/crystal.jpg",
+    price:"25"
   },
   {
     id: 7,
     name: "Cotton King Size Bed Sheets",
     category: "Bedding & Linen",
     description: "Soft 100% cotton sheets for king size beds, breathable and durable.",
-    image: "images2/cotton-double-bed-sheets7.jpg"
+    image: "images2/cotton-double-bed-sheets7.jpg",
+    price:"60"
   },
   {
     id: 8,
     name: "Dyson V15 Detect",
     category: "Cleaning & Home Care",
     description: "High-performance cordless vacuum cleaner with laser dust detection.",
-    image: "images2/dyson.jpg"
+    image: "images2/dyson.jpg",
+    price:"75"
   },
   {
     id: 9,
     name: "Wooden Table Lamp with Fabric Shade",
     category: "Home Decor",
     description: "Stylish bedside lamp with a wooden base and soft fabric shade.",
-    image: "images2/wood.jpg"
+    image: "images2/wood.jpg",
+    price:"40"
   }
 ];
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const links=document.getElementById("links")
 const user=document.getElementById("user")
 const user_info=document.getElementById("user-info")
 const productsDiv=document.getElementById("products");
+const items_added_Div=document.getElementById("item-added");
+
+
 
 function render(){
 if(localStorage.getItem("email")){
@@ -17,24 +20,55 @@ user_info.textContent=""
 }}
 render();
 
-const ss=products.map((item)=>console.log(item))
-console.log(ss)
+// const ss=products.map((item)=>console.log(item))
+// console.log(ss)
 
  function drawItem(){
-     productsDiv.innerHTML=products.map((item)=>{return `<div class="col-sm-12 col-md-4 col-lg-3 product border border-black rounded-2 mt-3 p-0  me-3">
+     productsDiv.innerHTML=products.map((item)=>{return `<div id=p${id}><div class="col-sm-12 col-md-4 col-lg-3 product border border-black rounded-2 mt-3 p-0  me-3">
                 <img class="w-100" src=${item.image} alt="">
         <div class="details text-center  d-flex flex-column justify-content-between">
             <div class="details-body  p-1 d-flex flex-column justify-content-center align-items-start row">
                 <h5 class="w-auto text-center">${item.name}</h5>
-                <h6 class="w-auto">Price: $<span class="price-span">400</span></h6>
+                <h6 class="w-auto">Price: $<span class="price-span">${item.price}</span></h6>
                 <h6 class="w-auto">Category: <span class="category-span">${item.category}</span></h6>
            </div>
-            <div class="btn-icon d-inline-flex justify-content-center gap-2 align-items-center mb-2">
+            <div class="btn-icon d-inline-flex justify-content-center gap-2 align-items-center mb-auto">
                 <i class="bi bi-heart-fill fs-3"></i>
-                <button class="p-1 rounded bg-primary-subtle fw-bolder fs-5">Add to cart</button>
+                <button class="p-1 btn-add rounded bg-primary-subtle fw-bolder fs-5" onclick=addToCart(${item.id})>Add to cart</button>
             </div>
        </div>
            
-    </div>`})
+    </div></div>`}).join("")
 }
-drawItem()
+drawItem();
+
+
+
+
+
+function addToCart(id){
+    let choosenItem=products.find((item)=>item.id===id);
+    let {product_id,name,category,description,image,price}=choosenItem;
+    let btn_add=document.querySelector(".btn-add");
+    if(btn_add.textContent==="Add to cart") 
+        
+    
+    {items_added_Div.innerHTML+=
+    `<div id="it-cart"><div id="tit-pr">
+        <p id="name">${name}</p>
+        <p id="price">price: $ ${price}<p/>
+    </div>
+    <div id="plus-minus"><i class="bi bi-file-minus"></i><span>1</span><i class="bi bi-file-plus"></i></div>
+    </div>
+    `
+    btn_add.textContent="Remove from cart";
+    btn_add.style.display="block";
+    btn_add.style.backgroundColor="red"
+
+}
+    else{
+        return
+    }
+    
+
+}
